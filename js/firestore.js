82a52d75bdc64e08fbeafb74e4cca50b673e34ce
Gerardo Jr Abantao <gerardojr.abantao@awsys-i.com>
2020-01-09 09:03:31 +0800

    firestore

---
+++ js/firestore.js
@@ -0,0 +1,31 @@
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyDEtbinbscaxVKpa6GAyrOxh9moQsXO9-U",
+    authDomain: "myfirstfirebaseproject-7cd16.firebaseapp.com",
+    databaseURL: "https://myfirstfirebaseproject-7cd16.firebaseio.com",
+    projectId: "myfirstfirebaseproject-7cd16",
+    storageBucket: "myfirstfirebaseproject-7cd16.appspot.com",
+    messagingSenderId: "970304867141",
+    appId: "1:970304867141:web:e8d354070d615e91504c2c",
+};
+
+firebase.initializeApp(config);
+let firestore = firebase.firestore();
+console.log("Cloud Firestores Loaded");
+
+// Enable offline capabilities
+firebase.firestore().enablePersistence()
+    .then(function() {
+        // Initialize Cloud Firestore through firebase
+        var db = firebase.firestore();
+    })
+    .catch(function(err) {
+        if (err.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled in one tab at a a time.
+
+        } else if (err.code == 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    });

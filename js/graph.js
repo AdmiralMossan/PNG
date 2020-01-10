10d803014d8a72f8152e8e26349a91ed1c93ee10
Renphil Ian Balantin <renphil.balantin@awsys-i.com>
2020-01-10 15:34:30 +0800

    pie

--- js/graph.js
+++ js/graph.js
@@ -5,28 +5,58 @@ function custom(x, y) {
     return (-Math.sin(x / Math.PI) * Math.cos(y / Math.PI) * 10 + 10) * 100;
 }
 
-async function getData(x, y){
+async function getData(x, y) {
     let count = 0;
     categories = ["A", "B", "C"];
-    await db.collection("reports").where( "category" , "==" , categories[x] ).where( "group" , "==" , y+1 ).get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            count+=1;
+    await db.collection("reports").where("category", "==", categories[x]).where("group", "==", y + 1).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            count += 1;
         });
-    }); 
+    });
     return count;
 
 
 }
 
-async function drawPie(){
-    
+async function loadData() {
+    var steps = 3;
+    var axisMax = 3;
+    var yAxisMax = 3;
+    var axisStep = axisMax / steps;
+    var z = 0;
+
+    let dataArray = [];
+
+    for (let i = 0; i < 3; i += axisStep) {
+        for (let j = 0; j < 3; j += axisStep) {
+            z = await getData((i / axisStep), (j / axisStep));
+            console.log(z);
+            dataArray.push({ x: i, y: j, z: z })
+        }
+    }
+
+
+    // Create and populate a data table.
+    data = new vis.DataSet();
+
+    for (let i = 0; i < dataArray.length; i++) {
+        data.add(dataArray[i]);
+    }
+
+    return data;
+}
+
+
+async function drawPie(data) {
     var myPieChart = new Chart(ctx, {
         type: 'pie',
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
                 label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [
+                    
+                ],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -47,88 +77,45 @@ async function drawPie(){
             }]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+
         }
     });
-	
+
 }
 
 // Called when the Visualization API is loaded.
-async function drawVisualization() {
+async function drawVisualization(data) {
+    console.log(data);
     var style = "bar";//document.getElementById("style").value;
     var withValue =
         ["bar-color", "bar-size", "dot-size", "dot-color"].indexOf(style) != -1;
 
-    // Create and populate a data table.
-    data = new vis.DataSet();
 
-    //x: Category, y: Group, z: Number
-    // all inputs are of data type Number
-    //var x = 0, y = 0, z = 0;
-    //data.add({ x: x, y: y, z: z });
-    // -----------remove this part-------
-    var steps = 3; 
-    var axisMax = 3;
-    var yAxisMax = 3;
-    var axisStep = axisMax / steps;
-    var z = 0;
-
-    dataArray = []
-    for(let i=0; i<3; i+=axisStep){
-        for(let j=0; j<3; j+=axisStep){
-            z = await getData((i/axisStep),(j/axisStep));
-            console.log(z);
-            dataArray.push({x: i, y: j , z: z})
-        }
-    }
-
-    for(let i=0; i<dataArray.length; i++){
-        data.add(dataArray[i]);
-    }
-    // for (var x = 0; x < axisMax; x += axisStep) {
-    //     for (var y = 0; y < yAxisMax; y += axisStep) {
-    //         //var z = custom(x, y);
-    //         z+=5;
-    //         if (withValue) {
-    //             var value = y - x;
-    //             data.add({ x: x, y: y, z: z, style: value });
-    //         } else {
-    //             data.add({ x: x, y: y, z: z });
-    //         }
-    //     }
-    // }
-    // -----------remove this part END-------
 
     // specify options
     var options = {
-        width: "100%",
-        height: "100%",
-        style: style,
-        showPerspective: false,
+        width: "90%",
+        height: "90%",
+        style: 'bar',
+        showPerspective: true,
         showGrid: true,
         showShadow: true,
+        animationPreload: true,
         xLabel: "Category",
         yLabel: "Groups",
         zLabel: "Number",
         xBarWidth: 0.5,
         yBarWidth: 0.5,
 
-
         // Option tooltip can be true, false, or a function returning a string with HTML contents
         //tooltip: true,
-        tooltip: function(point) {
+        tooltip: function (point) {
             // parameter point contains properties x, y, z
             return "Number: <b>" + point.z + "</b>";
         },
 
-        xValueLabel: function(value) {
-            switch(value){
+        xValueLabel: function (value) {
+            switch (value) {
                 case 0: return "A";
                 case 1: return "B";
                 case 2: return "C";
@@ -136,8 +123,8 @@ async function drawVisualization() {
             }
         },
 
-        yValueLabel: function(value) {
-            switch(value){
+        yValueLabel: function (value) {
+            switch (value) {
                 case 0: return "Group I";
                 case 1: return "Group II";
                 case 2: return "Group III";
@@ -145,7 +132,7 @@ async function drawVisualization() {
             }
         },
 
-        zValueLabel: function(value) {
+        zValueLabel: function (value) {
             return value;
         },
 
@@ -153,21 +140,20 @@ async function drawVisualization() {
         verticalRatio: 0.5
     };
 
-    var camera = graph ? graph.getCameraPosition() : null;
 
     // create our graph
     var container = document.getElementById("mygraph");
     graph = new vis.Graph3d(container, data, options);
 
-    if (camera) 
-        graph.setCameraPosition(camera); // restore camera position
+    graph.setCameraPosition({ horizontal: 0.0, vertical: 0.0, distance: 2 }); // restore camera position
 
     //document.getElementById("style").onchange = drawVisualization;
 }
 
 window.addEventListener("load", () => {
-    drawVisualization().then(function() {
-    drawPie();
-      });
+    loadData().then(function () {
+        drawVisualization(data);
+        drawPie(data);
+    });
 });
 

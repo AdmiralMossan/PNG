0026eccc1bb1a25530833760cc2fdb94184946b7
AdmiralMossan <colagami@gmail.com>
2020-01-10 09:18:15 +0800

    added js for modal control

--- js/button.js
+++ js/button.js
@@ -3,10 +3,12 @@ $( document ).ready(function() {
                 if (jQuery('#btn2')[0].hasAttribute('disabled')) {
                     $('#picture').attr('src', "Images/buttonD.jpg");
                     $('#picture').attr('disabled', true);
+                    $('#picture').attr('data-toggle', false);
                     $("#btn2").attr('disabled', false);
                     $("#btn3").attr('disabled', false);
                 }else{
                     $('#picture').attr('src', "Images/buttonA.jpg");
+                    $('#picture').attr('data-toggle', "modal");
                     $('#picture').attr('disabled', false);
                     $("#btn2").attr('disabled', true);
                     $("#btn3").attr('disabled', true);
@@ -16,10 +18,12 @@ $( document ).ready(function() {
                 if (jQuery('#btn3')[0].hasAttribute('disabled')) {
                     $('#picture').attr('src', "Images/buttonD.jpg");
                     $('#picture').attr('disabled', true);
+                    $('#picture').attr('data-toggle', false);
                     $("#btn1").attr('disabled', false);
                     $("#btn3").attr('disabled', false);
                 }else{
                     $('#picture').attr('src', "Images/buttonA.jpg");
+                    $('#picture').attr('data-toggle', "modal");
                     $('#picture').attr('disabled', false);
                     $("#btn1").attr('disabled', true);
                     $("#btn3").attr('disabled', true);
@@ -29,17 +33,38 @@ $( document ).ready(function() {
                 if (jQuery('#btn1')[0].hasAttribute('disabled')) {
                     $('#picture').attr('src', "Images/buttonD.jpg");
                     $('#picture').attr('disabled', true);
+                    $('#picture').attr('data-toggle', false);
                     $("#btn1").attr('disabled', false);
                     $("#btn2").attr('disabled', false);
                 }else{
                     $('#picture').attr('src', "Images/buttonA.jpg");
+                    $('#picture').attr('data-toggle', "modal");
                     $('#picture').attr('disabled', false);
                     $("#btn1").attr('disabled', true);
                     $("#btn2").attr('disabled', true);
                 }
             });
             $("#picture").click(function(){
+                if (jQuery('#picture1')[0].hasAttribute('disabled')) {
+                    $('#picture').attr('src', "Images/buttonD.jpg");
+                    $('#picture').attr('data-toggle', false);
+                    $('#picture').attr('disabled', true);
+                    $("#btn1").attr('disabled', false);
+                    $("#btn2").attr('disabled', false);
+                    $("#btn3").attr('disabled', false);
+                }else{
+                    $('#picture').attr('data-toggle', "modal");
+                    $('#picture').attr('src', "Images/buttonD.jpg");
+                    $('#picture').attr('disabled', true);
+                    $("#btn1").attr('disabled', false);
+                    $("#btn2").attr('disabled', false);
+                    $("#btn3").attr('disabled', false);
+                }
+            });
+
+            $("#closemodal").click(function(){
                 $('#picture').attr('src', "Images/buttonD.jpg");
+                $('#picture').attr('data-toggle', false);
                 $('#picture').attr('disabled', true);
                 $("#btn1").attr('disabled', false);
                 $("#btn2").attr('disabled', false);

011f820a634e0a8b301d371227dd741173841381
GerardoUbuntu <gaabantao1@up.edu.ph>
2020-01-14 15:29:15 +0800

    buttonFunctionalities

--- js/button.js
+++ js/button.js
@@ -1,64 +1,80 @@
-$( document ).ready(function() {
-            $("#btn1").click(function(){
-                if (jQuery('#btn2')[0].hasAttribute('disabled')) {
-                    $('#picture').attr('src', "Images/buttonD.jpg");
-                    $('#picture').attr('disabled', true);
-                    $('#picture').attr('data-toggle', false);
-                    $("#btn2").attr('disabled', false);
-                    $("#btn3").attr('disabled', false);
-                }else{
-                    $('#picture').attr('src', "Images/buttonA.jpg");
-                    $('#picture').attr('data-toggle', "modal");
-                    $('#picture').attr('disabled', false);
-                    $("#btn2").attr('disabled', true);
-                    $("#btn3").attr('disabled', true);
+var categories = []
+var buttons = []
+
+async function getCategories(){
+    await db.collection("categories").orderBy("name").get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+             categories.push(doc.data().name)
+        });
+    });
+    console.log(categories);
+}
+
+function initializeButtons(){
+    console.log(categories.length);
+    var Ctgcontainer = document.getElementById("categoryContainer");  
+    for(let i=0; i<categories.length; i++){
+        Ctgcontainer.innerHTML += `<div class="col-sm">
+             <button type="button" id="category` + categories[i] + `" class="col-sm btn btn-secondary w-75 mx-4 my-2"  style=" height: 120px;" >
+             Category ` + categories[i] + `</button>
+           </div>`;
+    }
+}
+
+function disabledButtons(i, j){
+    console.log(j.hasAttribute('disabled'));
+    if(j.hasAttribute('disabled')){
+        $('#picture').attr('src', "Images/buttonD.jpg");
+        $('#picture').attr('disabled', true);
+        $('#picture').attr('data-toggle', false);
+        for(let k=0; k<buttons.length; k++){
+            if(k!=i)
+                buttons[k].removeAttribute("disabled");
+           
+        }
+    }else{
+        $('#picture').attr('src', "Images/buttonA.jpg");
+        $('#picture').attr('data-toggle', "modal");
+        $('#picture').attr('disabled', false);
+        for(let k=0; k<buttons.length; k++){
+            if(k!=i){
+                buttons[k].setAttribute("disabled", "disabled");
+                getCategory(buttons[i].id.replace('category',''));
+            }        
+        }
+
+    }
+   
+}
+
+function disableAll(){
+    for(let i=0; i<buttons.length; i++){
+        buttons[i].removeAttribute("disabled");
+    }
+}
+
+
+$(document).ready(function() {
+            getCategories().then(function () {
+                initializeButtons();
+                buttons = $("button[id^='category']");
+                for(let i=0; i<buttons.length; i++){
+                    let j = i==buttons.length - 1 ? 0 : i+1;
+                    buttons[i].onclick = function(){disabledButtons(i, buttons[j])};
                 }
-            });
-            $("#btn2").click(function(){
-                if (jQuery('#btn3')[0].hasAttribute('disabled')) {
-                    $('#picture').attr('src', "Images/buttonD.jpg");
-                    $('#picture').attr('disabled', true);
-                    $('#picture').attr('data-toggle', false);
-                    $("#btn1").attr('disabled', false);
-                    $("#btn3").attr('disabled', false);
-                }else{
-                    $('#picture').attr('src', "Images/buttonA.jpg");
-                    $('#picture').attr('data-toggle', "modal");
-                    $('#picture').attr('disabled', false);
-                    $("#btn1").attr('disabled', true);
-                    $("#btn3").attr('disabled', true);
-                }
-            });
-            $("#btn3").click(function(){
-                if (jQuery('#btn1')[0].hasAttribute('disabled')) {
-                    $('#picture').attr('src', "Images/buttonD.jpg");
-                    $('#picture').attr('disabled', true);
-                    $('#picture').attr('data-toggle', false);
-                    $("#btn1").attr('disabled', false);
-                    $("#btn2").attr('disabled', false);
-                }else{
-                    $('#picture').attr('src', "Images/buttonA.jpg");
-                    $('#picture').attr('data-toggle', "modal");
-                    $('#picture').attr('disabled', false);
-                    $("#btn1").attr('disabled', true);
-                    $("#btn2").attr('disabled', true);
-                }
-            });
+            });;
+            
             $("#picture").click(function(){
                 if (jQuery('#picture')[0].hasAttribute('disabled')) {
                     $('#picture').attr('src', "Images/buttonD.jpg");
                     $('#picture').attr('data-toggle', false);
                     $('#picture').attr('disabled', true);
-                    $("#btn1").attr('disabled', false);
-                    $("#btn2").attr('disabled', false);
-                    $("#btn3").attr('disabled', false);
+                    disableAll();
                 }else{
                     $('#picture').attr('data-toggle', "modal");
                     $('#picture').attr('src', "Images/buttonD.jpg");
                     $('#picture').attr('disabled', true);
-                    $("#btn1").attr('disabled', false);
-                    $("#btn2").attr('disabled', false);
-                    $("#btn3").attr('disabled', false);
+                    disableAll();
                 }
             });
 
@@ -66,8 +82,6 @@ $( document ).ready(function() {
                 $('#picture').attr('src', "Images/buttonD.jpg");
                 $('#picture').attr('data-toggle', false);
                 $('#picture').attr('disabled', true);
-                $("#btn1").attr('disabled', false);
-                $("#btn2").attr('disabled', false);
-                $("#btn3").attr('disabled', false);
+                disableAll();
             });
         });

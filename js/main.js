70a8a85aaab597f1426efd8375b365154217ace2
Renphil Ian Balantin <renphil.balantin@awsys-i.com>
2020-01-14 09:58:34 +0800

    register

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-function proceed(){
-       $('#submit').toggle();
-       $('.groupContainer').toggle();
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmPassword");
+
+function validatePassword(){
+  if(password.value != confirmPassword.value) {
+       confirmPassword.setCustomValidity("Passwords Don't Match");
+  } else {
+       alert("Successully Registered");
+       window.location.replace("http://127.0.0.1:5500/login.html");
+  }
 }
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;

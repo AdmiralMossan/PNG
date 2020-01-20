69eb2feb1e594ce67426aebf49da190470578d9d
Renphil Ian Balantin <renphil.balantin@awsys-i.com>
2020-01-20 13:13:20 +0800

    download csv

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-$("#showReports").click(function () {
-  if($('#allReports').css("display") === "none"){
-    $("#allReports").show();
-    $('#showReports').text("Hide Reports")
-  }else if ($('#allReports').css("display") === "block"){
-    $("#allReports").hide();
-    $('#showReports').text("All Reports");
-  }
-});
-
 $(document).ready(function () {
   $(window).scroll(function () {
     var height = $(window).scrollTop();
@@ -17,13 +7,22 @@ $(document).ready(function () {
       $('.scroll-to-top').fadeOut();
     }
   });
-  $(document).ready(function () {
-    $(".scroll-to-top").click(function (event) {
-      event.preventDefault();
-      $("html, body").animate({ scrollTop: 0 }, "fast");
-      return false;
-    });
 
+  $(".scroll-to-top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+  });
+
+  $("#showReports").click(function () {
+    if ($('#allReports').css("display") === "none") {
+      $("#allReports").show();
+      $('#showReports').text("Hide Reports")
+    } else if ($('#allReports').css("display") === "block") {
+      $("#allReports").hide();
+      $('#showReports').text("All Reports");
+    }
   });
 
 })
+

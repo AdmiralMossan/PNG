01d7d62ec30a084cf9db0e8604d81af372bbc31f
Renphil Ian Balantin <renphil.balantin@awsys-i.com>
2020-01-29 14:41:18 +0800

    added notification badge

--- js/graph.js
+++ js/graph.js
@@ -228,11 +228,11 @@ async function reportsTable() {
     if (report.read === false) {
       notif = true;
       if (ctr === 0) {
-        $('#notifItem').html('<hr>');
+        $('#notifItem').html('<div class="px-4 py-0"><hr class="m-2 mb-3"></div>');
       }
       ctr += 1;
-      $('#notifDropdown').html('<i class="material-icons text-danger">notifications_active</i>')
-      $('#notifItem').append('<div class="dropdown-item py-0"><div class="row"><p class="col-12 text-danger m-0">New category "' + report.category + '" incident was reported.</p><p class="col-8 text-danger m-0 text-right"> date: ' + report.created.toDate().toLocaleString("en-US") + '</p><a class="ml-auto py-0" href="#" onClick= selectReport(' + report.id + ')>more details...</a></div><hr></div>')
+      $('#notifDropdown').html('<i class="material-icons text-danger">notifications_active</i><span class="badge badge-pill badge-danger p-1">' + ctr + '</span>')
+      $('#notifItem').append('<div class="dropdown-item py-0"><div class="row"><p class="col-12 text-danger m-0">New category "' + report.category + '" incident was reported.</p><p class="col-8 text-danger m-0"> date: ' + report.created.toDate().toLocaleString("en-US") + '</p><a class="ml-auto py-0" href="#" onClick= selectReport(' + report.id + ')>more details...</a></div><hr class="mt-1"></div>')
     }
   });
   if (notif === false) {

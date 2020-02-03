57925585a0d7b10b7d08bedd68a561385a343853
GerardoUbuntu <gaabantao1@up.edu.ph>
2020-02-03 15:13:14 +0800

    modal stack

--- js/reporting.js
+++ js/reporting.js
@@ -459,3 +459,22 @@ async function removeCategory(value){
     //     console.error("Error category deletion: ", error);
     // });
 }
+
+$(document).ready(function () {
+
+    $('#openBtn').click(function () {
+        $('#myModal').modal({
+            show: true
+        })
+    });
+
+        $(document).on('show.bs.modal', '.modal', function (event) {
+            var zIndex = 1040 + (10 * $('.modal:visible').length);
+            $(this).css('z-index', zIndex);
+            setTimeout(function() {
+                $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+            }, 0);
+        });
+
+
+});

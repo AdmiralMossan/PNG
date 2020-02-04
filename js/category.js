b679aa874491694a3ff4f9b28263b8986f29dc81
Renphil Ian Balantin <renphil.balantin@awsys-i.com>
2020-02-04 15:47:09 +0800

    fix category js scrollbar

--- js/category.js
+++ js/category.js
@@ -1,13 +1,14 @@
 window.addEventListener("load", async () => {
     isloaded = true;
     await showCategories();
-    
+
     $("#categoriesTable").DataTable({
         scrollY: 400
     });
-    
-    showCategories().then(function() {
-        $("#categoriesTable").DataTable();
+
+    showCategories().then(function () {
+        $("#categoriesTable").DataTable({ scrollY: 400 })
+
     });
 });
 
@@ -23,12 +24,12 @@ async function showCategories() {
                 tempCategories.push(doc.data());
             });
         });
-    
+
     $('#showCategoriesTable div').html("");
-    
+
     //Add body
     let head =
-        "<table id='categoriesTable' class='table table-striped table-responsive p-0 scroll-secondary col-12'>" +
+        "<table id='categoriesTable' class='display table table-striped table-responsive'>" +
         "<thead class='thead-inverse bg-custom text-custom'>" +
         "<tr>" +
         "<th style='width:25%;'>Category</th>" +
@@ -58,10 +59,10 @@ async function showCategories() {
 }
 
 async function addCategory() {
-    
+
     let name = document.getElementById("catName").value;
     let desc = document.getElementById("catDesc").value;
-    
+
     if (name == "" || desc == "")
         return
     let size = 0;
@@ -81,40 +82,40 @@ async function addCategory() {
         name: name,
         description: desc
     })
-    .then(async function () {
-        console.log("Document successfully written!");
-        sessionStorage.removeItem("category");
+        .then(async function () {
+            console.log("Document successfully written!");
+            sessionStorage.removeItem("category");
 
-        PNotify.success({
-            title: "Successfully added Category",
-            delay: 2000,
-            modules: {
-                Buttons: {
-                    closer: true,
-                    closerHover: true,
-                    sticker: false
-                },
-                Mobile: {
-                    swipeDismiss: true,
-                    styling: true
+            PNotify.success({
+                title: "Successfully added Category",
+                delay: 2000,
+                modules: {
+                    Buttons: {
+                        closer: true,
+                        closerHover: true,
+                        sticker: false
+                    },
+                    Mobile: {
+                        swipeDismiss: true,
+                        styling: true
+                    }
                 }
-            }
+            });
+
+            showCategories();
+            document.getElementById("catName").value = "";
+            document.getElementById("catDesc").value = "";
+
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
         });
-
-        showCategories();
-        document.getElementById("catName").value = "";
-        document.getElementById("catDesc").value = "";
-
-    })
-    .catch(function (error) {
-        console.error("Error writing document: ", error);
-    });
 
     $('#categoryModal').modal('hide');
 }
 
 async function updateCategory(value) {
-    
+
     let newName = document.getElementById("catName").value;
     let newDesc = document.getElementById("catDesc").value;
 
@@ -124,40 +125,40 @@ async function updateCategory(value) {
     db.collection("categories")
         .where("id", "==", value)
         .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
                 doc.ref.update({
                     name: newName,
                     description: newDesc
                 });
             });
-        
-        PNotify.success({
-            title: "Update Successful!",
-            delay: 2000,
-            modules: {
-                Buttons: {
-                    closer: true,
-                    closerHover: true,
-                    sticker: false
-                },
-                Mobile: {
-                    swipeDismiss: true,
-                    styling: true
+
+            PNotify.success({
+                title: "Update Successful!",
+                delay: 2000,
+                modules: {
+                    Buttons: {
+                        closer: true,
+                        closerHover: true,
+                        sticker: false
+                    },
+                    Mobile: {
+                        swipeDismiss: true,
+                        styling: true
+                    }
                 }
-            }
-        });
-        
-        showCategories();
-        document.getElementById("catName").value = "";
-        document.getElementById("catDesc").value = "";
-        
+            });
+
+            showCategories();
+            document.getElementById("catName").value = "";
+            document.getElementById("catDesc").value = "";
+
         })
-        .catch(function (error){
+        .catch(function (error) {
             console.error("Error category deletion: ", error);
         });
-    
-        $('#categoryModal').modal('hide');
+
+    $('#categoryModal').modal('hide');
 }
 
 async function removeCategory(value) {
@@ -191,35 +192,35 @@ async function removeCategory(value) {
         db.collection("categories")
             .where("id", "==", value)
             .get()
-            .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
                     doc.ref.delete();
                 });
-            
-            PNotify.success({
-                title: "Delete Successful!",
-                delay: 2000,
-                modules: {
-                    Buttons: {
-                        closer: true,
-                        closerHover: true,
-                        sticker: false
-                    },
-                    Mobile: {
-                        swipeDismiss: true,
-                        styling: true
+
+                PNotify.success({
+                    title: "Delete Successful!",
+                    delay: 2000,
+                    modules: {
+                        Buttons: {
+                            closer: true,
+                            closerHover: true,
+                            sticker: false
+                        },
+                        Mobile: {
+                            swipeDismiss: true,
+                            styling: true
+                        }
                     }
-                }
-            });
-            
-            showCategories();
-            
+                });
+
+                showCategories();
+
             })
-            .catch(function (error){
+            .catch(function (error) {
                 console.error("Error category deletion: ", error);
             });
     });
-    
+
 }
 
 // $(document).ready(function () {

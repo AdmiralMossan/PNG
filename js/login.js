ea96e1155b7814b8819fa019caa15f28aa2ed754
AdmiralMossan <colagami@gmail.com>
2020-02-07 15:58:34 +0800

    added comments

--- js/login.js
+++ js/login.js
@@ -1,8 +1,11 @@
 function displayLogin() {
+    // Declaration of Variables
     var x = document.getElementById("anonlogin");
     var y = document.getElementById("logindiv1");
     var z = document.getElementById("logindiv2");
+    // Hide initial Cards and Display Anon Login Card
     if (x.style.display === "none") {
+      // Fade out Animation
       z.style.animation = "fadeout .3s ease 0s forwards";
       y.style.animation = "fadeout .3s ease 0s forwards";
       setTimeout(function(){ 
@@ -10,11 +13,14 @@ function displayLogin() {
         y.style.display = "none";
         x.style.display = "block";
       },400);
-    } else {
-        z.style.animation = "fadein .3s ease 0s forwards";
-        y.style.animation = "fadein .3s ease 0s forwards";
-        z.style.display = "block";
-        y.style.display = "block";
-        x.style.display = "none";
+    } 
+    // Hide Anon Login Card and Display Initial Cards
+    else {
+      // Fade in animation
+      z.style.animation = "fadein .3s ease 0s forwards";
+      y.style.animation = "fadein .3s ease 0s forwards";
+      z.style.display = "block";
+      y.style.display = "block";
+      x.style.display = "none";
     }
   }

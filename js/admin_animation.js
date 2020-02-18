178bb3a698ce24010a78bc1fb2432b796d4029c7
AdmiralMossan <colagami@gmail.com>
2020-02-18 15:28:47 +0800

    added animation to report summary page

--- js/admin_animation.js
+++ js/admin_animation.js
@@ -1,15 +1,6 @@
 function load_content(){
-    var bg1 = document.getElementById("bg1");
-    var bg2 = document.getElementById("bg2");
-    var bg3 = document.getElementById("bg3");
-    var main = document.getElementById("maincontent");
+    var main = document.getElementById("stats-container");
     setTimeout(function(){ 
-      bg2.media = '';
-      setTimeout(function(){ 
-        bg3.media = '';
-        setTimeout(function(){ 
-          main.style.display='';
-        },600);
-      },600);
-    },600);
+      main.style = 'animation: fadein 1.5s ease 0s forwards;';
+    },1000);
   }
